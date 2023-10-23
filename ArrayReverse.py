68572d1c5c03695242adbb36f0a35b7ffbf30c3f
BTW075 <116366078+BTW075@users.noreply.github.com>
2023-10-23 19:16:42 +0530

    Create ArrayReverse.py

---
+++ ArrayReverse.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# initialising numpy array
+ini_array = np.array([1, 2, 3, 6, 4, 5])
+
+# using shortcut method to reverse
+res = np.flip(ini_array)
+
+# printing result
+print("initial array", ini_array)
+print("final array", str(res))
